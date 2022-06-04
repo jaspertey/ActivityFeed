9e817a36585c6092071887ac31742ed1f79a3616
Jasper Tey <jasper.tey@gmail.com>
2022-06-04 00:11:05 -0400

    Intermediate updates from legacy implementation.

--- src/ActivityFeed.php
+++ src/ActivityFeed.php
@@ -4,5 +4,24 @@ namespace JasperTey\ActivityFeed;
 
 class ActivityFeed
 {
-    // Build wonderful things
+    static $strings = [];
+    static $actions = [];
+    static $objects = [];
+
+    public static function addObjects($objects = [])
+    {
+        static::$objects = array_merge(static::$objects, $objects);
+    }
+
+    public static function strings($strings = [])
+    {
+        static::$strings = $strings;
+        return static::$strings;
+    }
+
+    public static function actions($actions = [])
+    {
+        static::$actions = $actions;
+        return static::$actions;
+    }
 }
